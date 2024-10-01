e35a701f6a52d055c91e89984cba2630173d6a85
ImmaBirdz <nakeza1375@hotmail.com>
2024-10-01 10:37:54 +0700

    Create TabTitle.jsx
    is a dynamic tab title

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import './css/Login.css'
 import Login from './component/Login';
 import Page from './component/Page';
 import Profile from './component/Profile';
@@ -12,8 +11,8 @@ function App() {
         <Route path="/" element={<Login />} />
         <Route path="/login" element={<Login />} />
         <Route path="/page" element={<Page />} />
-        <Route path="/profile" element={<Profile />} />
-        <Route path="*" element={<Page />} />
+        {/* <Route path="/profile" element={<Profile />} />
+        <Route path="*" element={<Page />} /> */}
       </Routes>
     </BrowserRouter>
   );
