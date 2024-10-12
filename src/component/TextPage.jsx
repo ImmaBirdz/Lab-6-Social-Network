f4bf734d42364a1c5bdc0afe4c353400bd4283d1
Malp <kanokwanplam89@gmail.com>
2024-10-12 23:57:31 +0700

    Complete scroll bar
    - finally my scroll bar can scrolled TT

--- src/component/TextPage.jsx
+++ src/component/TextPage.jsx
@@ -130,6 +130,88 @@ const TextPage = () => {
 
             </div>
 
+            <div className="postBox">
+
+                <div className="postedContent">
+
+                    <div className="userProf">
+
+                        <a href="#">
+                            <div className="userPics">
+                                <img width="40" height="40"
+                                    src="https://img.icons8.com/office/40/circled-user-female-skin-type-1-2.png"
+                                    alt="User Profile" />
+                            </div>
+                        </a>
+
+                        <div className="infoPost">
+                            <p className='postName'>nAme_user</p>
+                            {/* <p className='postDate'>Sep 35 3042, 00.01 am</p> */}
+                        </div>
+
+                    </div>
+
+                </div>
+
+                <p className="postText">
+                    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Laborum, labore.
+                </p>
+
+                <div className="postAction">
+
+                    <div className="activitiesIcons">
+
+                        <a href="#"><span className="iconAct">like <ion-icon name="heart-outline"></ion-icon></span></a>
+                        <a href="#"></a><span className="iconAct">comment <ion-icon name="chatbox-outline"></ion-icon></span>
+                        <a href="#"><span className="iconAct">repost <ion-icon name="repeat-outline"></ion-icon></span></a>
+
+                    </div>
+
+                </div>
+
+            </div>
+
+            <div className="postBox">
+
+                <div className="postedContent">
+
+                    <div className="userProf">
+
+                        <a href="#">
+                            <div className="userPics">
+                                <img width="40" height="40"
+                                    src="https://img.icons8.com/office/40/circled-user-female-skin-type-1-2.png"
+                                    alt="User Profile" />
+                            </div>
+                        </a>
+
+                        <div className="infoPost">
+                            <p className='postName'>nAme_user</p>
+                            {/* <p className='postDate'>Sep 35 3042, 00.01 am</p> */}
+                        </div>
+
+                    </div>
+
+                </div>
+
+                <p className="postText">
+                    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Laborum, labore.
+                </p>
+
+                <div className="postAction">
+
+                    <div className="activitiesIcons">
+
+                        <a href="#"><span className="iconAct">like <ion-icon name="heart-outline"></ion-icon></span></a>
+                        <a href="#"></a><span className="iconAct">comment <ion-icon name="chatbox-outline"></ion-icon></span>
+                        <a href="#"><span className="iconAct">repost <ion-icon name="repeat-outline"></ion-icon></span></a>
+
+                    </div>
+
+                </div>
+
+            </div>
+
         </div>
 
     );
