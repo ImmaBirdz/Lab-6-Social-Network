4bdfbeb7072478b03d4b59220c4e7f7eab368f34
ImmaBirdz <nakeza1375@hotmail.com>
2024-10-12 21:36:03 +0700

    [birb] Edit Profile
    fix width of profile

--- src/component/Profile.jsx
+++ src/component/Profile.jsx
@@ -13,75 +13,70 @@ const Profile = () => {
     }, []);
 
     return (
-        <div>
+        <div className="profileContainer">
 
-            <div className="profileContainer">
+            <div className="bioPart">
 
-                <div className="bioPart">
+                <div className="goBackTab">
 
-                    <div className="goBackTab">
-
-                        <span className="icon">
-                            <ion-icon name="caret-back-outline"></ion-icon>
-                        </span>
-
-                    </div>
-
-                    <div className="bioBox">
-
-                        <div className="leftBox">
-
-                            <div className="profPic"></div>
-
-                            <div className="accInfo">
-
-                                <div className="nameBox">
-                                    <div className="profName">Your Profile Name</div>
-                                </div>
-
-                                <div className="bioPfBox">
-                                    <div className="accBio">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Laborum, labore.</div>
-                                </div>
-
-                                <div className="accBox">
-                                    <div className="accProf">@Your Account Profile</div>
-                                </div>
-
-                            </div>
-
-                        </div>
-
-                        <div className="rightBox">
-
-                            <div className="accNum">
-                                <div className="postNum">0 post</div>
-                                <div className="followersNum"><a href="#">0 followers</a></div>
-                                <div className="followingNum"><a href="#">0 following</a></div>
-                            </div>
-                            
-                            <div className="editBtn"><a href="#">Edit Profile</a></div>
-
-                        </div>
-                        
-                    </div>
+                    <span className="icon">
+                        <ion-icon name="caret-back-outline"></ion-icon>
+                    </span>
 
                 </div>
 
-                <div className="line"></div>
+                <div className="bioBox">
 
-                <div className="actionPart">
+                    <div className="leftBox">
 
-                    <div className="actionBox">
-                        <button onClick={() => setActiveTab('Text')}>Text</button>
-                        <button onClick={() => setActiveTab('Media')}>Media</button>
+                        <div className="profPic"></div>
+
+                        <div className="accInfo">
+
+                            <div className="nameBox">
+                                <div className="profName">Your Profile Name</div>
+                            </div>
+
+                            <div className="bioPfBox">
+                                <div className="accBio">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Laborum, labore.</div>
+                            </div>
+
+                            <div className="accBox">
+                                <div className="accProf">@Your Account Profile</div>
+                            </div>
+
+                        </div>
+
                     </div>
-                    {activeTab === 'Text' && <TextPage />}
-                    {activeTab === 'Media' && <MediaPage />}
 
+                    <div className="rightBox">
+
+                        <div className="accNum">
+                            <div className="postNum">0 post</div>
+                            <div className="followersNum"><a href="#">0 followers</a></div>
+                            <div className="followingNum"><a href="#">0 following</a></div>
+                        </div>
+                            
+                        <div className="editBtn"><a href="#">Edit Profile</a></div>
+
+                    </div>
+                        
                 </div>
 
             </div>
 
+            <div className="line"></div>
+
+            <div className="actionPart">
+
+                <div className="actionBox">
+                    <button onClick={() => setActiveTab('Text')}>Text</button>
+                    <button onClick={() => setActiveTab('Media')}>Media</button>
+                </div>
+                {activeTab === 'Text' && <TextPage />}
+                {activeTab === 'Media' && <MediaPage />}
+
+            </div>
             <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
             <script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
 
