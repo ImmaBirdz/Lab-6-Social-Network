e157ac381320078b9ef711ab511f98dffbb80b90
ImmaBirdz <nakeza1375@hotmail.com>
2024-10-14 21:07:30 +0700

    Re-Create NotFound.jsx

---
+++ src/component/NotFound.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import { useNavigate } from 'react-router-dom';
+
+import '../css/NotFound.css'
+
+const NotFound = () => {
+
+    const navigate = useNavigate(); 
+  
+    const handleGoBack = () => {
+      navigate(-1); 
+    };
+
+
+    return (
+        <div className="NFcontainer">
+          <h1 className="NFheading">404</h1>
+          <p className="NFmessage">We couldn't find the page you are looking for.</p>
+          <button className="NFbutton" onClick={handleGoBack}>Go back to previous page</button>
+        </div>
+      );
+    };
+
+    
+export default NotFound;
