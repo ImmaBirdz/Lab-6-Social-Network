8fa13dbd5e0f5b31c0181a05e721a12bebb2e371
ImmaBirdz <nakeza1375@hotmail.com>
2024-10-16 02:35:41 +0700

    Edit comment and remove unnecessary hook

--- src/component/NavBar.jsx
+++ src/component/NavBar.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import '../css/Page.css'
 import { useContext } from 'react'
 import { LoginContext } from '../variable/LoginContext'
-import { Link, useNavigate } from 'react-router-dom'
+import { useNavigate } from 'react-router-dom'
 
 const NavBar = () => {
     const { loginID, setLoginID, setIsLogin } = useContext(LoginContext);
