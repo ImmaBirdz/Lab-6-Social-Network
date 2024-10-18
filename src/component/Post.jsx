5711207bb0cb5d778d2794da54a728bdfa0bd9f0
ImmaBirdz <nakeza1375@hotmail.com>
2024-10-19 00:06:00 +0700

    Update Post.*
    try to fetch post data to the web, adjusting position of text

--- src/component/Post.jsx
+++ src/component/Post.jsx
@@ -3,22 +3,33 @@ import '../css/Post.css';  // Separate CSS for styling posts
 import { TabTitle } from './TabTitle';
 import { LoginContext } from '../variable/LoginContext';
 import { db } from '../backend/firebaseConfig';
-import { collection, getDocs } from 'firebase/firestore';
+import { collection, getDocs, serverTimestamp } from 'firebase/firestore';
 
 const Post = () => {
-    const { postID, contextProfileID, contextPostID } = useContext(LoginContext);
-    const [selectedFriend, setSelectedFriend] = useState({
-        name: 'User ID',
-        imgSrc: 'https://via.placeholder.com/40',
-    });
-
+    const { postID } = useContext(LoginContext);
+    const { profileID, setProfileID } = useContext(LoginContext);
     const [ postData, setPostData ] = useState({}); // State for post data
-
-
-    const [postDate, setPostDate] = useState(new Date().toLocaleString()); // State for date and time
+    const [ profileData, setProfileData ] = useState({}); // State for profile data
+    const [ commentData, setCommentData ] = useState({}); // State for comment data
 
     useEffect(() => {
         TabTitle('Posts | Black Cat with Bow');
+    }, []);
+
+    // set profileID from profileData
+    useEffect(() => {
+        if (profileData.username) {
+            setProfileID(profileData.username);
+        }
+    }, [profileData]);
+
+    // set commentData from postData
+    useEffect(() => {
+
+        if (commentData === null) {
+            setCommentData(commentData);
+            console.log(commentData);
+        }
     }, []);
 
     useEffect(() => {
@@ -29,10 +40,37 @@ const Post = () => {
             postSnapshot.forEach(doc => {
                 if (doc.id === postID) {
                     setPostData(doc.data());
+                    // fetch comment data from post data
+                    fetchCommentData();
                 }
             });
         }
         fetchPostData();
+        // fetch profile data from post data
+        const fetchProfileData = async () => {
+            const userCollection = collection(db, 'user_data');
+            const userSnapshot = await getDocs(userCollection);
+            userSnapshot.forEach(doc => {
+                if (doc.id === postData.user_id) {
+                    setProfileData(doc.data());
+                }
+            });
+        }
+        fetchProfileData();
+
+        // fetch comment data from post data
+        const fetchCommentData = async () => {
+            const commentCollection = collection(db, 'comment');
+            const commentSnapshot = await getDocs(commentCollection);
+            commentSnapshot.forEach(doc => {
+                if (doc.id === postData.comments) {
+                    setCommentData(doc.data());
+                }
+            });
+        }
+        fetchCommentData();
+        console.log(commentData);
+
         TabTitle(`Post from ${postData.user_id} | Black Cat with Bow`);
     }, [postID, postData.user_id]);
 
@@ -73,19 +111,36 @@ const Post = () => {
         <div>
             <div className="container">
                 <main className="feed">
-                    <div className="post">
-                        {/* Post Author Information */}
-                        <div className="post-header">
-                            <img src={selectedFriend.imgSrc} alt="Post Author" className="profile-pic-large" />
-                            <span>{selectedFriend.name}</span>
+                    <div className="postBox">
+                        <div className="postedContent">
+                            <div className="userProf">
+                                <a href="#">
+                                    <div className="userPics">
+                                        <img style={{ 
+                                            backgroundImage: `url(${profileData.profile_pic})`, 
+                                            backgroundSize: '65px 65px',
+                                            }} />
+                                    </div>
+                                </a>
+                                <div>
+                                    <div className="infoPost">
+                                        <span className='postDisplayName' onClick={() => window.location.href = `/${profileID}`}><b><a>{profileData.display_name}</a></b></span>
+                                        <span className='postUsername' onClick={() => window.location.href = `/${profileID}`}>{`@${postData.user_id}`}</span>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
-
-                        <p>This is the content of the post. hehehehehehehhehehehe.</p>
-                        {/* Display Post Date and Time */}
-                        <p className="post-date">{postDate}</p>
+                        <p className="postText">{postData.input}</p>
+                        <div className="postAction">
+                            <div className="activitiesIcons">
+                            <ion-icon name="heart-outline"></ion-icon>
+                                <a href="#"></a><span className="iconAct">comment <ion-icon name="chatbox-outline"/></span>
+                                <a href="#"><span className="iconAct">repost <ion-icon name="repeat-outline"/></span></a>
+                            </div>
+                        </div>
                     </div>
 
-                    {/* Separator Line */}
+                        {/* Separator Line */ }
                     <hr className="post-separator" />
 
                     <div className="comment-section">
