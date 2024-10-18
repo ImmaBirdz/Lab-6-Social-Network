1283c71df8b583a406302e2949f3ffdd62c46657
ImmaBirdz <nakeza1375@hotmail.com>
2024-10-18 22:59:24 +0700

    Update NotFound.*
    fixed conflict css to others

--- src/component/NotFound.jsx
+++ src/component/NotFound.jsx
@@ -13,11 +13,13 @@ const NotFound = () => {
 
 
     return (
+      <div className='notfound-container'>
         <div className="NFcontainer">
           <h1 className="NFheading">404</h1>
           <p className="NFmessage">We couldn't find the page you are looking for.</p>
           <button className="NFbutton" onClick={handleGoBack}>Go back to previous page</button>
         </div>
+      </div>
       );
     };
 
