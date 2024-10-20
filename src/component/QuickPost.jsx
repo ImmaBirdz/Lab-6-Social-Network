9ce76b8d3a521af26079b4b94da5972485ff8cad
ImmaBirdz <nakeza1375@hotmail.com>
2024-10-20 16:25:43 +0700

    Update QuickPost
    can fetch data to post and can post to firebase, edit preview image, edit remove image button

--- src/component/QuickPost.jsx
+++ src/component/QuickPost.jsx
@@ -1,27 +1,45 @@
-import React, { useState, useContext } from 'react';
+import React, { useState, useContext, useEffect } from 'react';
 import '../css/QP.css';
 import { LoginContext } from '../variable/LoginContext'; // Adjust this xxx
+import { db } from '../backend/firebaseConfig';
+import { collection, doc, getDocs, addDoc, updateDoc, serverTimestamp } from 'firebase/firestore';
 
 const QuickPost = () => {
-  const { profileName, profilePic } = useContext(LoginContext);
+  const { profileID } = useContext(LoginContext);
+  const [ profileData, setProfileData ] = useState({}); // State profile data
+  const [ profilePic, setProfilePic ] = useState(null); // State profile pic
   const [isPostModalOpen, setIsPostModalOpen] = useState(false); // State main modal
-  const [post, setPost] = useState('');
+  const [postInput, setPostInput] = useState('');
   const [selectedImage, setSelectedImage] = useState(null);
   const [isEmojiPickerModalOpen, setIsEmojiPickerModalOpen] = useState(false); // State emoji modal
 
-  const userProfile = {
-    profileName: 'Catty chan',
-    profilePic: 'https://img.icons8.com/color/96/cat-profile.png',
-  };
+  // fetch profile id from context
+  useEffect(() => {
+    const fetchProfileData = async () => {
+      const userCollection = collection(db, 'user_data');
+      const userSnapshot = await getDocs(userCollection);
+      userSnapshot.forEach(doc => {
+        if (doc.id === profileID) {
+          setProfileData(doc.data());
+          setProfilePic(doc.data().profile_pic);
+        }
+      });
+    };
+    fetchProfileData();
+  }, [isPostModalOpen]);
 
+  // Main modal
   const togglePostModal = () => {
     setIsPostModalOpen(!isPostModalOpen);
   };
 
+  // Post input
   const handlePostChange = (e) => {
-    setPost(e.target.value);
+    setPostInput(e.target.value);
+
   };
 
+  // Image upload
   const handleImageChange = (e) => {
     const file = e.target.files[0];
     if (file) {
@@ -33,25 +51,59 @@ const QuickPost = () => {
     }
   };
 
+  // Remove image
   const handleRemoveImage = () => {
     setSelectedImage(null); // Remove the selected image
   };
 
+  // Emoji picker
   const handleEmojiClick = (emoji) => {
-    setPost((prevPost) => prevPost + emoji);
+    setPostInput((prevPost) => prevPost + emoji);
     setIsEmojiPickerModalOpen(false); 
   };
 
+  // Emoji picker modal
   const toggleEmojiPickerModal = () => {
     setIsEmojiPickerModalOpen((prev) => !prev);
   };
 
-  const handleSubmit = (e) => {
+  // Submit post
+  const handleSubmit =  async (e) => {
     e.preventDefault();
-    console.log('Profile Name:', profileName);
-    console.log('Post:', post);
-    console.log('Image:', selectedImage);
-    setPost('');
+
+    // Check if the input is empty
+    if (!postInput) {
+      alert('Please enter something before posting!');
+      return;
+    }
+    // Get the input value
+    const input = postInput;
+
+    // Get collection from firestore
+    const postCollection = collection(db, 'post');
+
+    // Add post info to payload
+    const newPostPayload = {
+      user_id: profileID,
+      input: input,
+      last_modified: serverTimestamp(),
+      number_of_comments: 0,
+      number_of_likes: 0,
+      number_of_repost: 0,
+      media: selectedImage ? selectedImage : ''
+    };
+    // Update the number of posts in user_data
+    const userDoc = doc(db, 'user_data', profileID);
+    await updateDoc(userDoc, {
+      number_of_posts: profileData.number_of_posts + 1
+    });
+    // Add post to firestore
+    addDoc(postCollection, newPostPayload).then((docRef) => {
+      alert('Post upload successfully!');
+    }).catch((error) => {
+      alert('Error adding document: ', error);
+    });
+    setPostInput('');
     setSelectedImage(null);
     setIsPostModalOpen(false);
     setIsEmojiPickerModalOpen(false); 
@@ -86,16 +138,19 @@ const QuickPost = () => {
             <div className="profile-section">
               <img
                 className="profile-pic"
-                src={userProfile.profilePic || 'https://img.icons8.com/color/96/cat_in_a_box.png'} 
+                src={profilePic} 
                 alt="Profile"
               />
-              <p>{userProfile.profileName}</p> 
+              <div className="profile-info">
+                <span className="postDisplayName">{profileData.display_name}</span>
+                <span className='postUserName'>{`@${profileData.username}`}</span>
+              </div>
             </div>
             
             <textarea
               className="post-input"
               placeholder="What's happening?"
-              value={post}
+              value={postInput}
               onChange={handlePostChange}
               required
             />
@@ -103,9 +158,9 @@ const QuickPost = () => {
             {/* Display selected image with a delete button */}
             {selectedImage && (
               <div className="image-preview">
-                <img src={selectedImage} alt="Selected" className="preview-image" />
+                <img src={selectedImage} alt="Selected" className="preview-image" style={{}} />
                 <button className="remove-image-button" onClick={handleRemoveImage}>
-                  X
+                  &#10006;
                 </button>
               </div>
             )}
