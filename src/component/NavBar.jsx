dd87ef9bf217eaac6975671ed4ce4061e498b89a
PraeMuay <Phonnaphat_chu@cmu.ac.th>
2024-10-24 01:10:55 +0700

    css and fontend

--- src/component/NavBar.jsx
+++ src/component/NavBar.jsx
@@ -5,6 +5,7 @@ import { LoginContext } from '../variable/LoginContext'
 import { db } from '../backend/firebaseConfig'
 import { collection, getDocs } from 'firebase/firestore'
 import { useNavigate } from 'react-router-dom'
+import  Logo  from '../img/Logo.png'
 
 const NavBar = () => {
     const { loginID, setLoginID, setIsLogin } = useContext(LoginContext);
@@ -45,7 +46,7 @@ const NavBar = () => {
                     <button className="nav-button" onClick={() => window.location.href = `/search/profile`}>Search</button>
                     <button className="nav-button" onClick={() => window.location.href = `/message/chat`}>Message</button>
                     <a href='/' className="icon" onClick={() => window.location.href = `/search/find`}>
-                        <img src="https://img.icons8.com/ios-glyphs/30/cat--v1.png" alt="Feed-icon" className="feed-icon" />
+                        <img src={Logo} alt="Feed-icon" className="feed-icon" />
                     </a>
                     <button className="nav-button" onClick={() => window.location.href = `/${loginID}`}>
                         <img src={profileData.profile_pic}
